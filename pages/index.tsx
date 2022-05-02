571d76653b9836c4fae1c4e1bdd53c371f4d1f3e
notAbx <74147411+asavor@users.noreply.github.com>
2022-05-02 04:31:13 +0100

    next+ts+tailwind

---
+++ pages/index.tsx
@@ -0,0 +1,14 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>decode</title>
+      </Head>
+    </div>
+  )
+}
+
+export default Home
